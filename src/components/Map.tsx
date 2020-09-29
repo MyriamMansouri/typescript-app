6add043a8f8322bacd60d7e0ec684c8212c0b38a
myriam mansouri <myriam.mansouri@gmail.com>
2020-09-29 12:54:29 -0400

    Changed map tile provider

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -13,14 +13,14 @@ const Map: React.FC = () => {
 
     const map = new OSMBuildings({
       container: 'map',
-      position: { latitude: 52.51836, longitude: 13.40438 },
+      position: { latitude: 45.5016889, longitude: 	-73.567256 },
       zoom: 16,
       minZoom: 15,
       maxZoom: 20,
       attribution: '© Map and Data <a href="http://osm.org/copyright">OpenStreetMap</a> contributors © 3D <a href="https://osmbuildings.org/copyright/">OSM Buildings</a>'
     });
     
-    map.addMapTiles("http://{s}.tile.osm.org/{z}/{x}/{y}.png");
+    map.addMapTiles(`https://api.mapbox.com/styles/v1/osmbuildings/cjt9gq35s09051fo7urho3m0f/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`);
     
     map.addGeoJSONTiles('https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json');
   }, [mapState]);
