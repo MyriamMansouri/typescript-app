35c106604c9b59326e2f635c86f037fe5d31aee9
myriam mansouri <myriam.mansouri@gmail.com>
2020-09-29 14:28:40 -0400

    Added marker to map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Marker, Popup } from "react-leaflet";
 import styled from "styled-components";
 
 const Map: React.FC = () => {
@@ -8,21 +9,35 @@ const Map: React.FC = () => {
   });
 
   React.useEffect(() => {
-
     const OSMBuildings = (window as any).OSMBuildings;
 
     const map = new OSMBuildings({
-      container: 'map',
-      position: { latitude: 45.5016889, longitude: 	-73.567256 },
+      container: "map",
+      position: { latitude: 45.5016889, longitude: -73.567256 },
       zoom: 16,
       minZoom: 15,
       maxZoom: 20,
-      attribution: '© Map and Data <a href="http://osm.org/copyright">OpenStreetMap</a> contributors © 3D <a href="https://osmbuildings.org/copyright/">OSM Buildings</a>'
+      tilt: 30,
+      fogColor: "blue",
     });
-    
-    map.addMapTiles(`https://api.mapbox.com/styles/v1/osmbuildings/cjt9gq35s09051fo7urho3m0f/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`);
-    
-    map.addGeoJSONTiles('https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json');
+
+    map.addMapTiles(
+      `https://api.mapbox.com/styles/v1/osmbuildings/cjt9gq35s09051fo7urho3m0f/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`,
+      {
+        color: "blue",
+        attribution:
+          '© Data <a href="http://openstreetmap.org/copyright/">OpenStreetMap</a> · © Map <a href="http://mapbox.com">Mapbox</a>',
+      }
+    );
+
+    map.addGeoJSONTiles(
+      "https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json"
+    );
+    map.addMarker(
+      { latitude: 45.5016889, longitude: -73.567256, altitude: 200 },
+      { color: 'rgb(0,60,100)'},
+      { scale: 1 }
+    );
   }, [mapState]);
 
   return (
@@ -34,5 +49,7 @@ const Map: React.FC = () => {
 
 const LeafletMap = styled.div`
   height: 100vh;
+  background-color: "grey";
 `;
+
 export default Map;
