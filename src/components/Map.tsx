bf6e3545187887cd7590954719caadcf46892a96
myriam mansouri <myriam.mansouri@gmail.com>
2020-10-01 23:33:04 -0400

    Changed map services to mapbox

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Marker, Popup } from "react-leaflet";
 import styled from "styled-components";
 
 const Map: React.FC = () => {
@@ -9,35 +8,70 @@ const Map: React.FC = () => {
   });
 
   React.useEffect(() => {
-    const OSMBuildings = (window as any).OSMBuildings;
-
-    const map = new OSMBuildings({
+    const mapboxgl = (window as any).mapboxgl;
+    mapboxgl.accessToken =
+    process.env.REACT_APP_MAPBOX_TOKEN;
+    const map = new mapboxgl.Map({
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [-10.0066, 40.7135],
+      zoom: 15.5,
+      pitch: 45,
+      bearing: -17.6,
       container: "map",
-      position: { latitude: 45.5016889, longitude: -73.567256 },
-      zoom: 16,
-      minZoom: 15,
-      maxZoom: 20,
-      tilt: 30,
-      fogColor: "blue",
+      antialias: true,
     });
 
-    map.addMapTiles(
-      `https://api.mapbox.com/styles/v1/osmbuildings/cjt9gq35s09051fo7urho3m0f/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`,
-      {
-        color: "blue",
-        attribution:
-          '© Data <a href="http://openstreetmap.org/copyright/">OpenStreetMap</a> · © Map <a href="http://mapbox.com">Mapbox</a>',
+    // The 'building' layer in the mapbox-streets vector source contains building-height
+    // data from OpenStreetMap.
+    map.on("load", function () {
+      // Insert the layer beneath any symbol layer.
+      const layers = map.getStyle().layers;
+
+      let labelLayerId;
+      for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === "symbol" && layers[i].layout["text-field"]) {
+          labelLayerId = layers[i].id;
+          break;
+        }
       }
-    );
- 
-    map.addGeoJSONTiles(
-      "https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json"
-    );
-    map.addMarker(
-      { latitude: 45.5016889, longitude: -73.567256, altitude: 200 },
-      { color: 'rgb(0,60,100)'},
-      { scale: 1 }
-    );
+
+      map.addLayer(
+        {
+          id: "3d-buildings",
+          source: "composite",
+          "source-layer": "building",
+          filter: ["==", "extrude", "true"],
+          type: "fill-extrusion",
+          minzoom: 15,
+          paint: {
+            "fill-extrusion-color": "#aaa",
+
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            "fill-extrusion-height": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "height"],
+            ],
+            "fill-extrusion-base": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "min_height"],
+            ],
+            "fill-extrusion-opacity": 0.6,
+          },
+        },
+        labelLayerId
+      );
+    });
   }, [mapState]);
 
   return (
